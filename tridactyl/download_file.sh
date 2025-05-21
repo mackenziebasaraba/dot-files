dec8c54e8a27cb17cc6323950415a4e697cfabb2
Peter MacKenzie-Basaraba <peter@mackenziebasaraba.ca>
2025-05-21 14:18:58 -0600

    Feat: Allow user to choose download directory
    Modify download_file.sh (;d) to allow the user to choose through a list
    of preset download locations through a dmenu prompt. Additional preset
    options can easily be added by modifying the OPTIONS variable or case
    statement.
    
    Current options:
        - Downloads (~/Downloads)
        - PDF (~/Downloads/PDF)
        - Videos (~/Videos)
        - Pictures (~/Pictures/ Saved\ Pictures)
    
    Further improvements:
        - An additional option that allows the user to write a file path
          from their home directory.

--- tridactyl/download_file.sh
+++ tridactyl/download_file.sh
@@ -1,15 +1,40 @@
 #!/bin/bash
-TARGET_DIR="$HOME/Downloads"
-mkdir -p "$TARGET_DIR"
-IMAGE_URL="$1"
-if [ -z "$IMAGE_URL" ]; then
-    notify-send -i "error" "No URL provided for download"
+
+OPTIONS="Downloads\nPDF\nVideos\nPictures"
+
+CHOICE=$(echo -e "$OPTIONS" | dmenu -i -p "Select download directory:" | cut -d'|' -f1)
+
+case "$CHOICE" in
+    "Downloads")
+        TARGET_DIR="$HOME/Downloads"
+        ;;
+    "PDF")
+        TARGET_DIR="$HOME/PDF"
+        ;;
+    "Videos")
+        TARGET_DIR="$HOME/Videos"
+        ;;
+    "Pictures")
+        TARGET_DIR="$HOME/Pictures/Saved Pictures"
+        ;;
+    *)
+        notify-send -i "error" "Download canceled."
+        exit 1
+        ;;
+esac
+
+mkdir -p "$TARGET_DIR" || { notify-send -i "error" "Failed to create directory."; exit 1; }
+
+FILE_URL="$1"
+if [ -z "$FILE_URL" ]; then
+    notify-send -i "error" "No URL provided for download."
     exit 1
 fi
-FILENAME=$(basename "$IMAGE_URL" | cut -d'?' -f1)
+
+FILENAME=$(basename "$FILE_URL" | cut -d'?' -f1)
 notify-send -i "info" "Starting download for $FILENAME"
-curl -Is "$IMAGE_URL" > /dev/null 2>&1 ||
+curl -Is "$FILE_URL" > /dev/null 2>&1 ||
     { notify-send -i "error" "Invalid URL"; exit 1; }
-wget -O "$TARGET_DIR/$FILENAME" "$IMAGE_URL" && \
-    notify-send -i "info" "Completed download for $FILENAME" || \
-    notify-send -i "error" "Download failed for $FILENAME"
+wget -O "$TARGET_DIR/$FILENAME" "$FILE_URL" && \
+    notify-send -i "info" "Completed download for $FILENAME." || \
+    notify-send -i "error" "Download failed for $FILENAME."
